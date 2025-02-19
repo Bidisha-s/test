d55c2b994312ab5a87dbeacff6b31711c652f58c
Bidisha Mondal/Digital Wallet /SRI-Bangalore/Associate/Samsung Electronics <bidisha.m@partner.samsung.com>
2025-02-19 10:28:10 +0530

    first commit

--- test.jsx
+++ test.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
-const test = () => {
-  return (
-    <div>test</div>
-  )
-}
-
-export default test
+curl --location 'https://in-analytics.mpay.samsung.com/api/session' \
+--header 'Content-Type: application/json' \
+--header 'Cookie: metabase.SESSION=abc42d34-4c00-4bd8-b9be-d5daa151ca83' \
+--data-raw '{
+    "username": "j.tejaswi@samsung.com",
+    "password":"tejaswi@99"
+}'
