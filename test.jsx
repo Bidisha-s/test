bd29481dae9bd9e924316cde4e370054d3dcea77
Bidisha Mondal/Digital Wallet /SRI-Bangalore/Associate/Samsung Electronics <bidisha.m@partner.samsung.com>
2025-02-21 16:06:11 +0530

    Initial commit

--- test.jsx
+++ test.jsx
@@ -5,16 +5,13 @@ import Signin from "../src/pages/signin/Signin.jsx"
 import List from "./pages/list/List";
 import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { useEffect, useState } from "react";
-import Navbar from "./components/navbar/Navbar.jsx";
+import { BrowserRouter, Routes, Route,Navigate } from "react-router-dom";
+import { useEffect,useState } from "react";
 
 
 function App() {
 
   const [isAuthenticated, setIsAuthenticated] = useState(false)
-
-
   useEffect(() => {
     const token = localStorage.getItem("token");
     if (token) setIsAuthenticated(true);
@@ -23,37 +20,26 @@ function App() {
 
 
   return (
-    <div className="app">
+    <div className={
+      "app"}>
+        
       <BrowserRouter>
-        {isAuthenticated ?
-
-          (
-            <>
-              <Routes>
-                <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/signin" />} />
-                <Route path="/promotions" element={isAuthenticated ? <List /> : <Navigate to="/signin" />} />
-                <Route path="/promotions/:id" element={isAuthenticated ? <Single /> : <Navigate to="/signin" />} />
-                <Route path="/promotions/new" element={isAuthenticated ? <New /> : <Navigate to="/signin" />} />
-
-                {/* Public Route (Signin) */}
-                <Route path="/signin" element={<Signin onSignin={() => setIsAuthenticated(true)} />} />
-
-                {/* Redirect all other routes */}
-                <Route path="*" element={<Navigate to={isAuthenticated ? "/" : "/signin"} />} />
-              </Routes>
-            </>
-          ) : (
-            <>
-
-            </>
-          )} {/* Show Navbar when logged in */}
         <Routes>
+          
+          {/* <Route path="/"> */}
+          
+            <Route path="/" element={isAuthenticated ? <Home /> : <Navigate to="/signin" />} />
+          <Route path="/promotions" element={isAuthenticated ? <List /> : <Navigate to="/signin" />} />
+          <Route path="/promotions/:id" element={isAuthenticated ? <Single /> : <Navigate to="/signin" />} />
+          <Route path="/promotions/new" element={isAuthenticated ? <New /> : <Navigate to="/signin" />} />
 
           {/* Public Route (Signin) */}
           <Route path="/signin" element={<Signin onSignin={() => setIsAuthenticated(true)} />} />
 
           {/* Redirect all other routes */}
           <Route path="*" element={<Navigate to={isAuthenticated ? "/" : "/signin"} />} />
+            
+          {/* </Route> */}
         </Routes>
       </BrowserRouter>
     </div>
